3d9460fd1b5c427bc06761617ceb15f612f099b5
@Jeff <Jmutugi2005@gmail.com>
2024-10-14 02:43:40 +0300

    Initial commit

---
+++ hello-1.py
@@ -0,0 +1,160 @@
+#x = 1
+#print(type(x))
+
+#y = 1.0
+#print(type(y))
+
+#z = True
+#print(type(z))
+
+#a ="Hello" + "World!"
+#Concantations
+#print(a)
+#print(type(a))
+
+#name = input("Enter your name:")
+#print(name)
+
+#print("How old are you?")
+#age = input()
+#print(age)
+
+#print("Welcome " + name + " You are now " + age + " appropriate")
+
+#y =input("Enter your name ")
+#print(type(y))
+
+#x = int(input("Enter a value "))
+#print(type(x))
+
+#z = 5
+#print("The number is " + str(z))
+
+#Calculate
+#first_number = int(input("Enter a number "))
+#second_number = int(input("Enter a number "))
+#print("The addition is ", + first_number + second_number)
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#sum = 1+ 2
+#print(sum)
+
+#from datetime import date
+#date.today()
+#print(date.today())
+
+#sys.argv is an array or a data structure that contains many items.
+#import sys
+#print(sys.argv)
+#print(sys.argv[0])
+#print(sys.argv[1])
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#a = 40
+#b = 30
+#test expression => The code below runs only if the test expression is true (a < b)
+#if a < b:
+    #run statement
+    #print(True)
+#else:
+    #print(False)
+
+#if test_expression:
+    #run statement
+
+#Using elif statements enables you to add multiple test expressions to your program.
+#j = 200
+#m = 200
+#if j > m: #j > m is the test expression
+    #print("j is less than or equal to m")
+#elif j == m:
+    #print("j is equal to m")
+
+#if test_expression:
+    # statement(s) to be run
+#elif test_expression:
+    # statement(s) to be run
+
+#z = 20
+#y = 20
+#if z > y:
+    #print("y is greater than z")
+#elif z < y:
+    #print("z is less than y")
+#else:
+    #print("z is equal to y")
+
+#a = 16
+#b = 25
+#c = 27
+#if a > b:
+    #if b > c:
+        #print ("a is greater than b and b is greater than c")
+    #else: 
+        #print ("a is greater than b and less than c")
+#elif a == b:
+    #print ("a is equal to b")
+#else:
+    #print ("a is less than b")
+
+
+
+#if test_expression:
+    # statement(s) to be run
+    #if test_expression:
+        # statement(s) to be run
+    #else: 
+        # statement(s) to be run
+#elif test_expression:
+    # statement(s) to be run
+    #if test_expression:
+        # statement(s) to be run
+    #else: 
+        # statement(s) to be run
+#else:
+    # statement(s) to be run
+
+#object_size = 10
+#if object_size > 5:
+    #print("We need to look at the object")
+#else:
+    #print("no threat posed")
+
+#a = 23
+#b = 34
+#if a == 34 or b == 34: #subexpression1 or subexpression2
+    #print(a + b)
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#floor division
+#seconds = 1042
+#display_minutes = 1042 // 60
+#display_seconds = 1042 % 60 #modulus
+#print(display_minutes)
+#print(display_seconds)
+
+#Converting strings to numbers
+#demo_int = int('22')
+#demo_float = float('23.002')
+#print(demo_int)
+#print(demo_float)
+
+#absolute values
+#print(abs(9-29))
+#print(abs(29-9))
+
+#Rounding function
+#print(round(1.4))
+#print(round(2.5))
+#print(round(2.7))
+
+#Math Library
+#from math import ceil, floor
+
+#round_up = ceil(12.5) # round up
+#print(round_up)
+
+#round_down = floor(12.5) #round down
+#print(round_down)
+
